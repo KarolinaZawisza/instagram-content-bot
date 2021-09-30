64489d29555df73100c5802f65226c90337d1c94
KarolinaZawisza <zawiszakarolina.t@gmail.com>
2021-09-30 02:31:54 +0200

    Getting targeted accounts and automatizing following + env

---
+++ envr.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv('C:/Users/zawis/Documents/EV/.env')
+
+INSTAGRAM_LOGIN = os.getenv('instagram_login')
+INSTAGRAM_PASSWORD = os.getenv('instagram_password')
--- instagram_manager.py
+++ instagram_manager.py
@@ -1,13 +1,56 @@
+import csv
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+from envr import INSTAGRAM_LOGIN, INSTAGRAM_PASSWORD
+
+
 class InstagramManager:
 
     def __init__(self, driver):
         self.driver = driver
 
     def log_in(self):
-        pass
+        sleep(1)
+        self.driver.find_element_by_xpath('/html/body/div[4]/div/div/button[1]').click()
+        self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(INSTAGRAM_LOGIN)
+        self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(INSTAGRAM_PASSWORD)
+        self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div').click()
+        sleep(5)
+        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+        self.driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]').click()
+
+    def quit(self):
+        self.driver.quit()
 
     def find_account(self, account_name: str):
+        sleep(2)
+        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[1]/div').click()
+        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input').send_keys(account_name)
+        sleep(1)
+        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input').send_keys(Keys.ENTER)
+        sleep(1)
+        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input').send_keys(Keys.ENTER)
+        sleep(3)
+
+    def open_followers(self):
+        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a').click()
+        sleep(2)
+
+    def get_followers(self):
+        # followers = []
+        for li in range(1, 3):
+            if self.driver.find_element_by_xpath(f'/html/body/div[6]/div/div/div[2]/ul/div/li[{li}]/div/div[3]/button').text == 'Follow':
+                self.driver.find_element_by_xpath(f'/html/body/div[6]/div/div/div[2]/ul/div/li[{li}]/div/div[3]/button').click()
+        #         followers.append(self.driver.find_element_by_xpath(f'/html/body/div[6]/div/div/div[2]/ul/div/li[{li}]/div/div[2]').text)
+        # return followers
+
+    @staticmethod
+    def save_followers_names(followers: list):
         pass
 
-    def follow_everyone(self):
+    def close_followers(self):
+        self.driver.find_element_by_xpath('/html/body/div[6]/div/div/div[1]/div/div[2]/button').click()
+        sleep(1)
+
+    def like_first_post(self):
         pass
